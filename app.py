e5f93a991bab845ec352351a9d88e907b887bb86
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-06-14 14:38:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
 import streamlit as st
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+from bs4 import BeautifulSoup
 
-@st.experimental_singleton
-def get_driver():
-    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# ------------- Settings for Pages -----------
+st.set_page_config(layout="wide")
 
-options = Options()
-options.add_argument('--disable-gpu')
-options.add_argument('--headless')
+# Keep text only
+def get_website_content(url):
+    driver = None
+    try:
+        # Using on Local
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920,1200')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                                  options=options)
+        st.write(f"DEBUG:DRIVER:{driver}")
+        driver.get(url)
+        time.sleep(5)
+        html_doc = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html_doc, "html.parser")
+        return soup.get_text()
+    except Exception as e:
+        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
+    finally:
+        if driver is not None: driver.quit()
+    return None
 
-driver = get_driver()
-driver.get('https://www.iana.org/help/example-domains')
 
-st.code(driver.page_source)
+
+
+# ---------------- Page & UI/UX Components ------------------------
+def main_sidebar():
+    # 1.Vertical Menu
+    st.header("Running Selenium on Streamlit Cloud")
+    site_extraction_page()
+
+
+def site_extraction_page():
+    SAMPLE_URL = "https://spectrum.ieee.org/3d-printed-rocket"
+    url = st.text_input(label="URL", placeholder="https://example.com", value=SAMPLE_URL)
+
+    clicked = st.button("Load Page Content",type="primary")
+    if clicked:
+        with st.container(border=True):
+            with st.spinner("Loading page website..."):
+                content = get_website_content(url)
+                st.write(content)
+
+
+if __name__ == "__main__":
+    main_sidebar()
