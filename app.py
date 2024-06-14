af4882d1ba6265ada92f0f60817500b7a0ee0b21
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-06-14 14:33:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -9,46 +9,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-def my_function():
-    st.write("Button has been clicked!")
-    # Add any additional logic you want to run when the button is clicked
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    
-    driver = get_driver()
-    driver.get('https://www.iana.org/help/example-domains')
-    
-    st.code(driver.page_source)
-    return "Function executed successfully!"
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
 
-def load_data(data):
-    return pd.read_csv(data)
-    
-def main():
-    st.set_page_config(layout="wide")
+driver = get_driver()
+driver.get('https://www.iana.org/help/example-domains')
 
-    st.subheader("Mapa PML")
-    menu = ["Inicio","Sobre nosotros"]
-
-    choice = st.sidebar.selectbox("Menu",menu)
-
-    if choice == "Inicio":
-        # Streamlit application
-        st.title("Streamlit Button Click Example")
-
-        st.write("Click the button below to run the function.")
-
-        if st.button("Run Function"):
-            result = my_function()
-            st.write(result)
-
-    else:
-        st.subheader("Data")
-        df = load_data("data.csv")
-        st.dataframe(df)
-
-
-
-if __name__ == "__main__":
-    main()
+st.code(driver.page_source)
