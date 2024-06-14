1dcdce35b4c225dc674af3142444449a352cf23b
alcamilo1207 <alcamilopez@gmail.com>
2024-06-14 17:47:27 +0200

    updated site

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Inicio":
-        info = "Data source: Bundesnetzagentur | SMARD.de\nSMARD receives the data directly from the European Network of Transmission System Operators for Electricity (ENTSO-E). \nOnly data verified by the Bundesnetzagentur is published on SMARD. \nThe Bundesnetzagentur is constantly exchanging information with the transmission network operators (TSOs) in order to continuously improve data quality. More info: https://www.smard.de/en/datennutzung"
-        st.write(info)
+        st.write("Data source: Bundesnetzagentur | SMARD.de\n","More info: https://www.smard.de/en/datennutzung")
         # Create a dataframe
         df = jr.get_prices()
         st.dataframe(df)
