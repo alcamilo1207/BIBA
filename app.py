e3d0728c84480e7de372a1f683df0348d78e3fe8
alcamilo1207 <alcamilopez@gmail.com>
2024-06-14 17:38:31 +0200

    modification to the page

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ import json_req as jr
 def main():
     st.set_page_config(layout="wide")
 
-    st.subheader("Energy prices")
+    st.subheader("Day-ahead energy prices forecast")
+
     menu = ["Inicio","Sobre nosotros"]
 
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Inicio":
+        info = """
+            SMARD receives the data directly from the European Network of Transmission System Operators for Electricity (ENTSO-E). Only data verified by the Bundesnetzagentur is published on SMARD. The Bundesnetzagentur is constantly exchanging information with the transmission network operators (TSOs) in order to continuously improve data quality.
+        """
+        st.write(info)
         # Create a dataframe
         df = jr.get_prices()
         st.dataframe(df)
