aaba388d8060dc6e660b88046653ecba44dd1eca
alcamilo1207 <alcamilopez@gmail.com>
2024-06-14 18:25:24 +0200

    updated app

--- app.py
+++ app.py
@@ -6,26 +6,23 @@ import streamlit.components.v1 as components
 
 
 def main():
-    st.set_page_config(layout="wide")
-
-    st.subheader("Day-ahead energy prices forecast")
-
-    menu = ["Inicio","Sobre nosotros"]
+    menu = ["Energy prices data","Scheduler"]
 
     choice = st.sidebar.selectbox("Menu",menu)
 
     if choice == "Inicio":
+        st.subheader("Day-ahead energy prices forecast")
         st.write("Data source: Bundesnetzagentur | SMARD.de. More info: https://www.smard.de/en/datennutzung")
         # Create a dataframe
         df = jr.get_prices()
         st.dataframe(df)
 
-        fig = shelp.get_schedulePlot()
-        fig_html = mpld3.fig_to_html(fig)
-        components.html(fig_html, height=600)
-        #st.pyplot(fig,use_container_width=True)
     else:
         st.subheader("Data")
+        fig = shelp.get_schedulePlot()
+        fig_html = mpld3.fig_to_html(fig)
+        components.html(fig_html, width=1200,height=600)
+        #st.pyplot(fig,use_container_width=True)
 
 
 
