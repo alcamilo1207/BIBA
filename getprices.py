c0241b98d2f6c2ec5171c9928451a0e465e8f708
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-06-14 14:07:00 +0200

    Update getprices.py

--- getprices.py
+++ getprices.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import streamlit as st
 import os, sys
 
 @st.experimental_singleton
