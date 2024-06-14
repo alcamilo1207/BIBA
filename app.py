ad276b41852ef566d271923f37480c2ccacd8422
alcamilo1207 <alcamilopez@gmail.com>
2024-06-14 15:00:24 +0200

    updated imports

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 from bs4 import BeautifulSoup
