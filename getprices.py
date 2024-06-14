46ae5787d43878833b10b099c46b4cbd2adad4fe
alcamilo1207 <81839026+alcamilo1207@users.noreply.github.com>
2024-06-14 14:01:49 +0200

    Update getprices.py

--- getprices.py
+++ getprices.py
@@ -2,20 +2,38 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import os, sys
 
-# Configure Chrome options
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run in headless mode
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_experimental_option("prefs", {
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True,
-    "download.default_directory": r"C:\Users\Camilo Lopez-Salazar\Desktop\code"  # Change this to your desired download directory
-    #"download.default_directory": r"C:\Users\Camilo Laptop\PycharmProjects\ATLimplementation\BIBA\Austing"  # Change this to your desired download directory
-})
+@st.experimental_singleton
+def installff():
+  os.system('sbase install geckodriver')
+  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+
+_ = installff()
+from selenium import webdriver
+from selenium.webdriver import FirefoxOptions
+
+def ff():
+    opts = FirefoxOptions()
+    opts.add_argument("--headless")
+    browser = webdriver.Firefox(options=opts)
+    
+    browser.get('http://example.com')
+    st.write(browser.page_source)
 
 def scrap_data():
+    # Configure Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_experimental_option("prefs", {
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True,
+        "download.default_directory": r"C:\Users\Camilo Lopez-Salazar\Desktop\code"  # Change this to your desired download directory
+        #"download.default_directory": r"C:\Users\Camilo Laptop\PycharmProjects\ATLimplementation\BIBA\Austing"  # Change this to your desired download directory
+    })
+    
     # Initialize the webdriver
     driver = webdriver.Chrome(options=chrome_options)
 
