3ca050ca1ae9a04e15d86aac232d978018b59b25
alcamilo1207 <alcamilopez@gmail.com>
2024-06-18 15:02:07 +0200

    More date-related error fixed

--- json_req.py
+++ json_req.py
@@ -7,7 +7,24 @@ import numpy as np
 
 region_col_name = "Germany/Luxembourg [€/MWh] Original resolutions"
 
+def get_price_at_minute(hourly_price_values,minute):
+    h = int(minute/1440)
+    price = hourly_price_values[h]
+    return price
+
+def create_empty_energy_prices_df(start_datetime):
+    date_range_start = pd.date_range(start_datetime.strftime("%Y/%m/%d"), periods=24, freq='h')
+    date_range_end = pd.date_range(start=date_range_start[1], periods=24, freq='h')
+    df = pd.DataFrame(np.full((24,3),0.0),columns=['Start date','End date', region_col_name])
+    df['Start date'] = date_range_start
+    df['End date'] = date_range_end
+    return df
+
 def get_prices(date):
+    """"
+    Request energy marked data through the www.smard.de API.
+    """
+
     # Define the URL to send the request to
     url = 'https://www.smard.de/nip-download-manager/nip/download/market-data'  # Replace with the actual URL
 
@@ -18,10 +35,12 @@ def get_prices(date):
     }
 
     # Get the current day datetime starting at 00:00 hours
-    current_day = datetime(date.year, date.month, date.day, 0, 0, 0)
+    picked_day = datetime(date.year, date.month, date.day)
 
     # Get the next day datetime starting at 00:00 hours (2 hours shift corrected)
-    start_datetime = current_day + timedelta(days=1) #- timedelta(hours=2)
+    start_datetime = picked_day + timedelta(days=1) #- timedelta(hours=2)
+    print("start_datetime",start_datetime)
+    st.write(start_datetime)
 
     # Get the other next day datetime starting at 00:00 hours
     end_datetime = start_datetime + timedelta(days=1)
@@ -29,7 +48,6 @@ def get_prices(date):
     # Converting datetime to integer format
     start_date = int(start_datetime.timestamp()*1000)
     end_date = int(end_datetime.timestamp()*1000)
-
 
     # Define the data to be sent in the request
     data = {"request_form":[{"format":"CSV","moduleIds":[8004169,8004170,8000251,8005078,8000252,8000253,8000254,8000255,8000256,8000257,8000258,8000259,8000260,8000261,8000262,8004996,8004997],"region":"DE","timestamp_from":start_date,"timestamp_to":end_date,"type":"discrete","language":"en","resolution":"hour"}]}
@@ -49,65 +67,51 @@ def get_prices(date):
         list = [x.split(';') for x in csv_str.split('\n')]
         list[0][0] = 'Start date'
         # Convert string to data frame
-        df = pd.DataFrame(list[1:])
+        df = pd.DataFrame(list[1:-1])
 
         # Check if dataset is empty
-        date_range_start = pd.date_range(datetime.now().strftime("%Y/%m/%d"), periods=24, freq='h')
-        date_range_end = pd.date_range(date_range_start[1], periods=24, freq='h')
         if df.iloc[0,0] == 'No data for submitted query\r':
-            df = pd.DataFrame(np.full((24,3),0.0),columns=['Start date','End date', region_col_name])
-            df['Start date'] = date_range_end
-            df['End date'] = date_range_end
-            return df
+            return create_empty_energy_prices_df(start_datetime)
 
-
-        # Dropping last row
+        # Adding the headings
         df.columns = list[0][:df.shape[1]]
-        df = df.drop(df.index[-1])
 
         return df
     else:
         # Unsuccessful request
         st.write(f"Request failed with status code: {response.status_code}")
         st.write("Response content:", response.text)
-        return None
-
-def get_price_at_minute(price_hourly_values,minute):
-    h = int(minute/1440)
-    price = price_hourly_values[h]
-    return price
+        return create_empty_energy_prices_df(start_datetime)
 
 def calculate_energy_cost(prices_df,job_schedule,power_schedule):
-    if prices_df.shape == (24,19):
-        prices_str = prices_df[region_col_name].values
-        try:
-            prices = [float(item) for item in prices_str]
-        except Exception as e:
-            if str(e) == "could not convert string to float: '-'":
-                prices = np.full(24,0.0)
-                print("Day-ahead prices not available")
-            else:
-                print(e)
-    else:
-        prices = np.full(24,0.0)
-
+    # Start all the output variables at zero to avoid unassigned variables
+    performance, total_cost, total_energy, total_production, total_number_of_jobs = 0,0,0,0,0 
+    
+    # Getting variables for internal operations
     power_values = power_schedule.values/(60*1000)
     energies = job_schedule['energy'].values
     sizes = job_schedule['size'].values
 
-    total_cost = np.sum([pow[0]*get_price_at_minute(prices,min) for min, pow in enumerate(power_values)])
-
-    total_number_of_jobs = 0
+    # Counting number of jobs
     for size in sizes:
         if size > 0:
             total_number_of_jobs += 1
 
-    total_production = sizes.sum()
-    total_energy = energies.sum()
+    total_production += sizes.sum()
+    total_energy += energies.sum()
 
+    # First check if the dataframe has the right shape
     if prices_df.shape == (24,19):
-        performance = total_production/(total_energy*total_cost)
-    else:
-        performance = 0.0
+        prices_str = prices_df[region_col_name].values
+        try:
+            # Convert prices array values from strings to floats
+            prices = [float(item) for item in prices_str]
+            total_cost += np.sum([pow[0]*get_price_at_minute(prices,min) for min, pow in enumerate(power_values)])
+            performance += total_production/(total_energy*total_cost)
+        except Exception as e:
+            if str(e) == "could not convert string to float: '-'":
+                print("Day-ahead prices not available")
+            else:
+                print(e)
 
     return performance, total_cost, total_energy, total_production, total_number_of_jobs
